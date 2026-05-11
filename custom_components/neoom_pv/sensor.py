a0e5cfaabe08b0c5c5f2a96748e11c9948835969
Bauxi231 <BAUXI231@users.noreply.github.com>
2026-05-11 06:49:04 +0200

    style: Remove unused import in sensor.py

--- custom_components/neoom_pv/sensor.py
+++ custom_components/neoom_pv/sensor.py
@@ -11,7 +11,6 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 _LOGGER = logging.getLogger(__name__)
 
